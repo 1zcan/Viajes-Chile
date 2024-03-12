ee955f9a9e20c66dddb59d6b89d3cd2e3d64cdf4
1zcan <orlandoaravenav568@gmail.com>
2024-03-12 18:35:49 -0300

    Commit final, Comentarios en el Codigo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,19 @@
 $(document).ready(function () {
-    $("#Enviar").click(function () {
+
+    $("#Enviar").click(function () {                                   /*Alerta al enviar correo*/
         alert("El correo fue enviado correctamente");
       });
-      $("#Quien").click(function () {
+
+      $("#Quien").click(function () {                         /* toggles para ocultar el contenido de las secciones al hacer click en el titulo de cada una */
         $("#tarjetas").toggle();;
       });
+
       $("#destacado").click(function () {
         $("#tarjetas2").toggle();;
       });
+
       $("#Contactar").click(function () {
         $("#labels").toggle();;
       });
 });
+    
